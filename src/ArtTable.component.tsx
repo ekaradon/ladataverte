904e7c1bdaf39aa9a7ec53e4542b18aeb6832104
Denis Zimmermann <denis.zimmermann@comwatt.com>
2021-07-28 19:34:02 +0200

    feat: add a filter on the name of the art work

--- src/ArtTable.component.tsx
+++ src/ArtTable.component.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import { Table } from 'evergreen-ui'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { artWorkEntity } from './ArtWork.entity'
@@ -12,27 +13,32 @@ const RowLink = styled(Link)({
 const { data } = sample
 
 function ArtTable() {
+  const [filter, setFilter] = useState('')
+
   return (
     <Table>
       <Table.Head>
-        <Table.SearchHeaderCell flex={5} />
+        <Table.SearchHeaderCell onChange={setFilter} value={filter} flex={5} />
         <Table.TextHeaderCell flex={1}>Date</Table.TextHeaderCell>
         <Table.TextHeaderCell flex={3}>tags</Table.TextHeaderCell>
       </Table.Head>
       <Table.Body maxHeight='50%'>
-        {data.map(artWorkEntity).map((artWork) => (
-          <RowLink key={artWork.id} to={`/artwork/${artWork.id}`}>
-            <Table.Row isSelectable>
-              <Table.TextCell flex={5}>{artWork.title}</Table.TextCell>
-              <Table.TextCell flex={1} isNumber>
-                {artWork.dateStart}
-              </Table.TextCell>
-              <Table.TextCell flex={3} textProps={{ color: 'muted' }}>
-                {artWork.tags.join(', ')}
-              </Table.TextCell>
-            </Table.Row>
-          </RowLink>
-        ))}
+        {data
+          .map(artWorkEntity)
+          .filter((artWork) => !filter || artWork.title.includes(filter))
+          .map((artWork) => (
+            <RowLink key={artWork.id} to={`/artwork/${artWork.id}`}>
+              <Table.Row isSelectable>
+                <Table.TextCell flex={5}>{artWork.title}</Table.TextCell>
+                <Table.TextCell flex={1} isNumber>
+                  {artWork.dateStart}
+                </Table.TextCell>
+                <Table.TextCell flex={3} textProps={{ color: 'muted' }}>
+                  {artWork.tags.join(', ')}
+                </Table.TextCell>
+              </Table.Row>
+            </RowLink>
+          ))}
       </Table.Body>
     </Table>
   )
